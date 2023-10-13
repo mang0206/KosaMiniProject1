891c9cd2595eb1bfea3aa13246f40559baaf2665
jeon-aelim <devaelim@gmail.com>
2023-10-13 09:30:49 +0900

    setting

--- unimeeting/src/main/java/com/example/unimeeting/UnimeetingApplication.java
+++ unimeeting/src/main/java/com/example/unimeeting/UnimeetingApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude= DataSourceAutoConfiguration.class)
+@SpringBootApplication//(exclude= DataSourceAutoConfiguration.class)
 @ComponentScan(basePackages = {"com.example.unimeeting", "thymeleaf.exam"})
-//@MapperScan(value={"com.example.unimeeting.dao"})
+@MapperScan(value={"com.example.unimeeting.dao"})
 public class UnimeetingApplication {
 
 	public static void main(String[] args) {
