487efd8eae289650a86b4574b279088d2aaaf35b
mang0206 <alswosp0206@naver.com>
2023-10-13 14:56:48 +0900

    Connet Cloudtype for mariaDB

--- unimeeting/src/main/java/com/example/unimeeting/UnimeetingApplication.java
+++ unimeeting/src/main/java/com/example/unimeeting/UnimeetingApplication.java
@@ -7,10 +7,9 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication//(exclude= DataSourceAutoConfiguration.class)
-@ComponentScan(basePackages = {"com.example.unimeeting", "thymeleaf.exam"})
+@ComponentScan(basePackages = {"com.example.unimeeting"})
 @MapperScan(value={"com.example.unimeeting.dao"})
 public class UnimeetingApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UnimeetingApplication.class, args);
 	}
