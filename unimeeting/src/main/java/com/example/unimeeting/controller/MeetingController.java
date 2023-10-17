0c194683b191aba8d894f06dc3394d5b5d7cf3b1
jeon-aelim <devaelim@gmail.com>
2023-10-17 12:04:45 +0900

    meeting completed

--- unimeeting/src/main/java/com/example/unimeeting/controller/MeetingController.java
+++ unimeeting/src/main/java/com/example/unimeeting/controller/MeetingController.java
@@ -1,7 +1,10 @@
 package com.example.unimeeting.controller;
 
 import com.example.unimeeting.dao.MeetingMapper;
+import com.example.unimeeting.dao.MeetingMemberMapper;
 import com.example.unimeeting.domain.MeetingDTO;
+import com.example.unimeeting.domain.MeetingMemberDTO;
+import com.example.unimeeting.domain.UserVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,11 +14,14 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.List;
 
 @Controller
+@RequestMapping("/meeting")
 public class MeetingController {
 
     @Autowired
     MeetingMapper metmap;
 
+    @Autowired
+    MeetingMemberMapper mmm;
     // Get Category
     @RequestMapping(value = "/getCategory", produces = "application/json; charset=utf-8" )
     @ResponseBody
@@ -27,7 +33,7 @@ public class MeetingController {
     // Meeting Board
     // ctgr(category), path -> default all
     // search query
-        @RequestMapping(value = {"/meeting/board" ,"/meeting/board/{ctgr}"})
+        @RequestMapping(value = {"" ,"/{ctgr}"})
         public ModelAndView viewMetBoard(@PathVariable(required = false) String ctgr,@RequestParam(required = false) String search, @RequestParam(defaultValue = "1") int page){
         ModelAndView mv = new ModelAndView();
         mv.addObject("ctgr_list", getCategory());
@@ -39,7 +45,7 @@ public class MeetingController {
     }
 
     // Insert Meeting Post
-    @PostMapping("/meeting/insertMet")
+    @PostMapping("/insertMet")
     public String insertMet(MeetingDTO meetingDTO, Model m){
         boolean result = metmap.insertMet(meetingDTO);
         if (!result) {
@@ -49,7 +55,7 @@ public class MeetingController {
     }
 
     // view Meeting Post
-    @GetMapping("/meeting/post")
+    @GetMapping("/post")
     public ModelAndView viewMetPost(int idx){
         MeetingDTO meeting = metmap.viewMetPost(idx);
         int meeting_member = metmap.countMetMem(idx);
@@ -63,20 +69,20 @@ public class MeetingController {
     }
 
     // get Meeting
-    @RequestMapping(value = "/meeting/getMetJson", produces = "application/json; charset=utf-8")
+    @RequestMapping(value = "/getMetJson", produces = "application/json; charset=utf-8")
     @ResponseBody
     public MeetingDTO getMetJson(int idx){
         return metmap.viewMetPost(idx);
     }
 
     // delete meeting
-    @RequestMapping("/meeting/deleteMet")
+    @RequestMapping("/deleteMet")
     public String deleteMetPost(int idx, String writer_nickname){ // HttpSession
         return metmap.deleteMeeting(idx, writer_nickname) ? "redirect:/meeting/board" : "redirect:/";
     }
 
     // update meeting
-    @RequestMapping("/meeting/updateMet")
+    @RequestMapping("/updateMet")
     public String updateMetPost(MeetingDTO meetingDTO,Model m){
         System.out.println(meetingDTO);
         boolean result = metmap.updateMet(meetingDTO);
@@ -84,5 +90,66 @@ public class MeetingController {
             m.addAttribute("msg", "글 작성을 처리하는 동안 오류 발생");
         }
         return "redirect:/meeting/board";
+    }
+
+    // session, user_idx,
+    @RequestMapping("/apply")
+    public ModelAndView insertMetMem(int meeting_idx, @ModelAttribute("user") UserVO user){
+        ModelAndView mv = viewMetPost(meeting_idx);
+
+        if(user.getIdx() == 0){
+            mv.addObject("msg", "로그인 후 이용 가능한 서비스입니다. ");
+        }else{
+            MeetingMemberDTO meetingMemberDTO = new MeetingMemberDTO(meeting_idx, user.getIdx());
+            if(mmm.insertMetMem(meetingMemberDTO)){
+                mv.addObject("msg", "신청이 완료되었습니다.");
+            }else {
+                mv.addObject("msg", false);
+            }
+        }
+        mv.setViewName("MetPostView");
+        return mv;
+    }
+
+    @RequestMapping("/apply/cancel")
+    public ModelAndView deleteMetMemByMem(int meeting_idx,@ModelAttribute("user") UserVO user){
+        ModelAndView mv = viewMetPost(meeting_idx);
+        if(user.getIdx() == 0){
+            mv.addObject("msg", "로그인 후 이용 가능한 서비스입니다. ");
+        }else{
+            if(mmm.deleteMetMem(meeting_idx,user.getIdx())){
+                mv.addObject("msg", "취소가 완료되었습니다.");
+            }else {
+                mv.addObject("msg", false);
+            }
+        }
+        mv.setViewName("MetPostView");
+        return mv;
+    }
+
+    @RequestMapping("/accept")
+    public ModelAndView updateMetMem(int meeting_idx, int user_idx){
+        ModelAndView mv = viewMetPost(meeting_idx);
+        // 권한 필요
+        if(mmm.deleteMetMem(meeting_idx,user_idx)){
+            mv.addObject("msg", "취소가 완료되었습니다.");
+        }else {
+            mv.addObject("msg", false);
+        }
+        mv.setViewName("MetPostView");
+        return mv;
+    }
+    @RequestMapping("/accept/cancel")
+    public ModelAndView deleteMetMemByLeader(int meeting_idx, int user_idx){
+        ModelAndView mv = viewMetPost(meeting_idx);
+        // 권한 필요
+        if(mmm.updateApplyMetMem(meeting_idx,user_idx)){
+            mv.addObject("msg", "취소가 완료되었습니다.");
+        }else {
+            mv.addObject("msg", false);
+        }
+        mv.setViewName("MetPostView");
+        return mv;
+
     }
 }
--- unimeeting/src/main/java/com/example/unimeeting/dao/MeetingMemberMapper.java
+++ unimeeting/src/main/java/com/example/unimeeting/dao/MeetingMemberMapper.java
@@ -1,12 +1,20 @@
 package com.example.unimeeting.dao;
 
 import com.example.unimeeting.domain.MeetingMemberDTO;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface MeetingMemberMapper {
 
     @Insert("insert into meeting_member (meeting_idx, user_idx, accepted) values (#{meeting_idx} , #{user_idx}, false)")
     public boolean insertMetMem(MeetingMemberDTO meetingMemberDTO);
+
+    @Delete("delete from meeting_member where meeting_idx=#{meeting_idx} and user_idx=#{user_idx}")
+    public boolean deleteMetMem(int meeting_idx, int user_idx);
+
+    @Update("update meeting_member set accepted=true where meeting_idx=#{idx} and user_idx=#{user_idx}")
+    public boolean updateApplyMetMem(int meeting_idx, int user_idx);
 }
