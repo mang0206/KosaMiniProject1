03935c7cbfd6145b9b576fbf004d5e792dfef328
HAPALPAL <ehddnsla0102@naver.com>
2023-10-17 12:03:47 +0900

    userLogin,userRegister

--- unimeeting/src/main/java/com/example/unimeeting/domain/UserVO.java
+++ unimeeting/src/main/java/com/example/unimeeting/domain/UserVO.java
@@ -1,5 +1,18 @@
 package com.example.unimeeting.domain;
 
-public class UserVO
-{
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+public class UserVO {
+    private int idx;
+    private String user_id;
+    private String password;
+    private String nickname;
+    private String email;
+    private String category;
+    private String phone_number;
+
 }
