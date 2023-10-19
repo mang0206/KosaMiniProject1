26baf221851255137a611b1bd3f7235e1cb39b8e
hapalpal <ehddnsla0102@naver.com>
2023-10-19 11:05:35 +0900

    board

---
+++ unimeeting/src/main/java/com/example/unimeeting/domain/NoticeVO.java
@@ -0,0 +1,19 @@
+package com.example.unimeeting.domain;
+
+import java.sql.Date;
+
+
+import lombok.Data;
+
+@Data
+public class NoticeVO {
+    private int idx;
+    private String title;
+    private String content_text;
+    private Date created_datetime;
+    private String writer_nickname;
+    private String type;//공지사항의 글인지 게시판의 글인지 구별하기위한 것.
+
+    private String nickname;
+    private String user_id;
+}
