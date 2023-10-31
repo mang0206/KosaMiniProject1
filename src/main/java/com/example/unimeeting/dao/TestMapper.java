e14a77f5f4424fb63df428d8ae932d689c0efc2a
jeon-aelim <devaelim@gmail.com>
2023-10-31 17:39:45 +0900

    .git move

--- src/main/java/com/example/unimeeting/dao/TestMapper.java
+++
@@ -1,4 +0,0 @@
-package com.example.unimeeting.dao;
-
-public interface TestMapper {
-}
--- src/main/java/com/example/unimeeting/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.unimeeting.service;
-
-import com.example.unimeeting.domain.UserVO;
-
-public interface UserService {
-    UserVO getUserByUserId(String userId);
-    void registerUser(UserVO user);
-    boolean authenticateUser(String userId, String password);
-}
